4ca4efdcc76d03f99cd5a7d88c5a6ab623fefb4e
Cole <43682943+cole-easton@users.noreply.github.com>
2023-07-07 10:46:51 -0400

    Remove border colors from graph

--- main2.js
+++ main2.js
@@ -28,42 +28,42 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
                 label: 'SensERIE',
                 data: json.SensERIE.occupants,
                 borderColor: "#00ace0",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },{
                 label: 'BiteSizeScience',
                 data:json.BiteSizeScience.occupants,
-                borderColor:"rgba(0,255,0,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"rgba(0,255,0,1)",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },
             {
                 label: 'CummingsRoom',
                 data:json.CummingsRoom.occupants,
-                borderColor:"rgba(255,255,0,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"#172e47",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },
             {
                 label: 'ExploreYou',
                 data:json.ExploreYou.occupants,
-                borderColor:"rgba(100,100,0,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"rgba(100,100,0,1)",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },
             {
                 label: 'Artifacts',
                 data:json.Artifacts.occupants,
-                borderColor:"rgba(0,100,100,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"rgba(0,100,100,1)",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },
             {
                 label: 'Biodiversity',
                 data:json.Biodiversity.occupants,
-                borderColor:"rgba(255,0,0,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"rgba(255,0,0,1)",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             },
             {
                 label: 'RethinkExtinct',
                 data:json.RethinkExtinct.occupants,
-                borderColor:"rgba(0,0,255,0.3)",
-                backgroundColor: "rgba(255, 255, 255, 0.4)"
+                borderColor:"rgba(0,0,255,1)",
+                backgroundColor: "rgba(255, 255, 255, 0)"
             }
             ],
             labels: []
