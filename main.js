94839cb052ed72cd68cd8c7d812dad06ced90a13
Eric Scott <escott1245@gmail.com>
2023-07-07 10:33:46 -0400

    "multi graph"

--- main.js
+++ main.js
@@ -4,29 +4,69 @@ import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.j
 Chart.defaults.backgroundColor = "white";
 Chart.defaults.color = "#00ace0";
 
-const sampleRoom = new Room("Sample Room", 5000);
-
-document.querySelector("#enter").onclick = _ => sampleRoom.registerEntrance();
-//document.querySelector("#enter").onclick = _ => updateGraph(sampleRoom.getTimeSeriesData());
-document.querySelector("#exit").onclick = _ => sampleRoom.registerExit();
-document.querySelector("#update").onclick = _ => updateGraph(sampleRoom.getTimeSeriesData());
-
+const sampleRoom = new Room("Sample Room", 500);
 
 const canvas = document.querySelector("#chart");
 const ctx = canvas.getContext("2d");
 
-function updateGraph(data) {
-    console.log(data);
-    let chart = new Chart(ctx, {
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+(async _ => {
+    for (let i = 0; i < 20; i++) {
+        const data = sampleRoom.getTimeSeriesData();
+        updateGraph(data);
+        await delay(1000);
+    }
+})()
+
+  let chart;
+  fetch('./RoomData.json').then(response => response.json()).then(json => {
+    console.log(json);
+    chart = new Chart(canvas, {
         type: 'line',
         data: {
             datasets: [{
-                label: 'First dataset',
-                data: data,
+                label: 'SensERIE',
+                data: json.SensERIE.occupants,
                 borderColor: "#00ace0",
                 backgroundColor: "rgba(255, 255, 255, 0.4)"
-            }],
-            labels: data.map((_,i)=>i)
+            },{
+                label: 'BiteSizeScience',
+                data:json.BiteSizeScience.occupants,
+                borderColor:"rgba(0,255,0,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            },
+            {
+                label: 'CummingsRoom',
+                data:json.CummingsRoom.occupants,
+                borderColor:"rgba(255,255,0,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            },
+            {
+                label: 'ExploreYou',
+                data:json.ExploreYou.occupants,
+                borderColor:"rgba(100,100,0,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            },
+            {
+                label: 'Artifacts',
+                data:json.Artifacts.occupants,
+                borderColor:"rgba(0,100,100,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            },
+            {
+                label: 'Biodiversity',
+                data:json.Biodiversity.occupants,
+                borderColor:"rgba(255,0,0,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            },
+            {
+                label: 'RethinkExtinct',
+                data:json.RethinkExtinct.occupants,
+                borderColor:"rgba(0,0,255,0.3)",
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            }
+            ],
+            labels: []
         },
         options: {
             scales: {
@@ -34,9 +74,19 @@ function updateGraph(data) {
                     suggestedMin: 50,
                     suggestedMax: 100
                 }
-            }
+            },
         }
     });
+   })
 
+
+function updateGraph(data) {
+    if (!chart)
+        return;
+    console.log(chart.data.datasets[0].data);
+    const currentLength = chart.data.datasets[0].data.length;
+    chart.data.datasets[0].data.push(...data.filter((_,i)=>i>=currentLength));
+    chart.data.labels = data.map((_,i)=>i);
+    chart.update();
+    console.log("hohoho");
 }
-
